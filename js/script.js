e97c749abd62e3832196867c62a61330900fcdf0
Andrey Aratov <madingame@yandex.ru>
2017-07-02 17:02:31 +0300

    Main page slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    console.log('Alive!!!');
+$(document).ready(function () {
+	console.log('Alive!!!');
+	callSlider();
 });
+
+function callSlider() {
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		center: true,
+		autoplay: true
+	});
+}
