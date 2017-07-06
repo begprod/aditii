bd0ad33081f3f721a873fc5f3dae6b343759672b
Andrey Aratov <madingame@yandex.ru>
2017-07-06 13:47:34 +0300

    mobile menu & footer accordion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 	callSliders();
+	showMobileMenu();
+	showFooterLinks();
 });
 
 function callSliders() {
@@ -25,5 +27,19 @@ function callSliders() {
 					items: 1
 				}
 			}
+	});
+}
+
+function showMobileMenu() {
+	$('.menu-trigger').click(function() {
+		$('.menu__wrapper').slideToggle(300);
+	});
+}
+
+
+function showFooterLinks() {
+	$('.footer__head').click(function() {
+		$('.footer__links:visible').slideUp(300);
+		$(this).parent().find('.footer__links').slideToggle(300);
 	});
 }
